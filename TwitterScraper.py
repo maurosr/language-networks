ed3c2b94dbf5664e20137965e5cf77fc286b3225
Mauro Schilman <mschilman@medallia.com>
2019-05-06 00:22:37 -0300

    Working PoC

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -1,7 +1,10 @@
+import itertools
+from collections import Counter
 import networkx as nx
-import requests
-from requests_oauthlib import OAuth2Session
+import nltk
 from oauthlib.oauth2 import BackendApplicationClient
+import requests
+from requests_oauthlib import OAuth2, OAuth2Session
 
 from Scrapper import Scraper
 from credentials import CLIENT_ID, CLIENT_SECRET
@@ -16,16 +19,40 @@ class TwitterScraper(Scraper):
     CLIENT = BackendApplicationClient(client_id=CLIENT_ID)
     OAUTH = OAuth2Session(client=CLIENT)
 
+    nltk.download('punkt')
+
     def __init__(self):
         super().__init__()
         self._network = nx.Graph()
+
+    def scrape(self, lang='es'):
+        token = TwitterScraper._authenticate()
+        auth = OAuth2(CLIENT_ID, TwitterScraper.CLIENT, token=token)
+        params = {'q': 'a', 'lang': lang, 'count': 100}
+
+        r = requests.get(TwitterScraper.SEARCH_URL, params=params, auth=auth)
+        c = Counter()
+        for text in TwitterScraper._get_tweets_text(r):
+            for sentence in nltk.sent_tokenize(text, language='spanish'):
+                for word1, word2 in itertools.product(nltk.word_tokenize(sentence, language='spanish'), repeat=2):
+                    word1, word2 = min(word1, word2), max(word1, word2)
+                    c[(word1, word2)] += 1
+
+        for (word1, word2), weight in c.items():
+            nx.add_path(self._network, [word1, word2], weight=weight)
+        return self._network
 
     @staticmethod
     def _authenticate():
         return TwitterScraper.OAUTH.fetch_token(token_url=TwitterScraper.AUTH_URL, client_id=CLIENT_ID,
                                                 client_secret=CLIENT_SECRET)
 
-    def scrape(self):
-        token = TwitterScraper._authenticate()
-        nx.add_path(self._network, [0, 1], weight=7)
-        return self._network
+    @staticmethod
+    def _get_tweets_text(r):
+        jr = r.json()
+        if 'errors' in jr:
+            print(jr['errors'])
+            exit(1)
+
+        for tweet in jr['statuses']:
+            yield tweet['text']
